dabba12606c9b5f1df691d245bd67f2146f69ae6
sabari.r <sabari.r@ivymobility.com>
2025-09-29 16:31:57 +0530

    CarryFwd&Subarray

--- src/DSA1/Array/SlidingWindow/CountingSubarray.java
+++ src/DSA1/Array/SlidingWindow/CountingSubarray.java
@@ -1,6 +1,6 @@
 package DSA1.Array.SlidingWindow;
 
-//Problem Description
+//Problem Description //
 //
 //Given an array A of N non-negative numbers and a non-negative number B,
 //you need to find the number of subarrays in A with a sum less than B.
